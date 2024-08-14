602517e2e180852b04994320ab0d1a739d5e9bec
imenemedjaoui <mimi15mimo@gmail.com>
2024-08-14 14:29:08 +0200

    wip: 2nd attempt to change articles promo display

--- eshop/ecommerce/admin.py
+++ eshop/ecommerce/admin.py
@@ -106,8 +106,8 @@ admin.site.register(Categorie)
 admin.site.register(SousCategorie)
 @admin.register(Promo)
 class PromoAdmin(admin.ModelAdmin):
+    filter_horizontal = ('articles',)
     list_display = ('libelle', 'discount_percentage', 'end_date')
-    filter_horizontal = ('articles')
 
 #####################################################################################
 # EDIT-ONE-FIELD-ONLY TABLES
