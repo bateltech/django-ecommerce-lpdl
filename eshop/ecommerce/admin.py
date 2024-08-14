414a1e506a52e6bb53a099108bed8ae132f2e4a9
imenemedjaoui <mimi15mimo@gmail.com>
2024-08-14 23:37:05 +0200

    wip: add delete permission to admin

--- eshop/ecommerce/admin.py
+++ eshop/ecommerce/admin.py
@@ -165,8 +165,8 @@ class ClientUserAdmin(UserAdmin):
         ('Dates importantes', {'fields': ('last_login', 'date_joined')}),
     )
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
     def has_change_permission(self, request, obj=None):
         return False
